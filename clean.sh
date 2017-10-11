16679a04aceb43a18e545e82e13b4c7dca3ca701
SofiaAparicio <sofia.aparicio@tecnico.ulisboa.pt>
2017-10-11 12:19:57 +0100

    Transducer 2 and 3 done

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rm *.fst *.pdf
+rm letras_transdutor.txt transdutor2_final.txt transdutor3_final.txt
--- run.sh
+++ run.sh
@@ -33,15 +33,58 @@ fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait invert_final.fst |
 
 #################
 
-#################Transdutor 1
+#################
+#Transdutor 1
 python compact2fst.py letras.txt > letras_transdutor.txt
 fstcompile --isymbols=syms.sym --osymbols=syms.sym letras_transdutor.txt | fstarcsort > letras_transdutor.fst
 fstcompile --isymbols=syms.sym --osymbols=syms.sym black_space.txt | fstarcsort > black_space.fst
 
 ###operacoes transdutores
 fstconcat letras_transdutor.fst black_space.fst > letras_transdutor1.fst
-fstunion invert_final.fst letras_transdutor1.fst > transdutor1_union.fst
+fstconcat invert_final.fst black_space.fst > invert_final_space.fst
+fstunion invert_final_space.fst letras_transdutor1.fst > transdutor1_union.fst
 fstclosure transdutor1_union.fst > transdutor1.fst
 
 ###print transdutor1
 fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait transdutor1.fst | dot -Tpdf  > transdutor1.pdf
+
+#################
+#Transdutor 2
+python compact2fst.py transdutor2.txt > transdutor2_final.txt
+fstcompile --isymbols=syms.sym --osymbols=syms.sym transdutor2_final.txt | fstarcsort > transdutor2_final.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait transdutor2_final.fst | dot -Tpdf  > transdutor2_final.pdf
+
+#################
+#Transdutor 3
+python compact2fst.py transdutor3.txt > transdutor3_final.txt
+fstcompile --isymbols=syms.sym --osymbols=syms.sym transdutor3_final.txt | fstarcsort > transdutor3_final.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait transdutor3_final.fst | dot -Tpdf  > transdutor3_final.pdf
+
+#################
+#Descodificador
+fstarcsort --sort_type=ilabel transdutor1.fst > transdutor1sort.fst
+fstintersect transdutor1sort.fst transdutor2_final.fst > r.fst 
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait r.fst | dot -Tpdf  > r.pdf
+
+
+
+
+#################
+#################
+#Testes
+#teste 1o transdutor
+fstcompile --isymbols=syms.sym --osymbols=syms.sym  99.txt | fstarcsort > 99.fst
+
+fstcompose 99.fst transdutor1.fst > XCIX.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait XCIX.fst | dot -Tpdf  > XCIX.pdf
+
+# teste 2o transdutor
+fstcompile --isymbols=syms.sym --osymbols=syms.sym  XCIX_trans2.txt | fstarcsort > XCIX_trans2.fst
+
+fstcompose XCIX_trans2.fst transdutor2_final.fst > 3513.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait 3513.fst | dot -Tpdf  > 3513.pdf
+
+
+
+
+####
