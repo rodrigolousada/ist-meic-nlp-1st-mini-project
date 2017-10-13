4e3f4255a67a45987401249652f5e77595439ae3
Rodrigo <rodrigo.m.lousada@gmail.com>
2017-10-13 22:34:14 +0100

    Bug in Transducer 3 to fix!

--- run.sh
+++ run.sh
@@ -118,6 +118,16 @@ fstcompile --isymbols=syms.sym --osymbols=syms.sym  tests/test_Vr_tX79m_.txt | f
 python word2fst.py iiimmmbii_iirirriiri_ > tests/test_iiimmmbii_iirirriiri_.txt
 fstcompile --isymbols=syms.sym --osymbols=syms.sym  tests/test_iiimmmbii_iirirriiri_.txt | fstarcsort > tests/test_iiimmmbii_iirirriiri_.fst
 
+python word2fst.py biblioteca_do_palacio_de_monserrate_na_estante_a_esquerda_no_dia_14_de_novembro_pelas_10_23_de_maio_ > tests/test_cod_email.txt
+fstcompile --isymbols=syms.sym --osymbols=syms.sym  tests/test_cod_email.txt | fstarcsort > tests/test_cod_email.fst
+
+python word2fst.py Xs_21_pm_32111_d9_d9z97r0_3332111_3412_312_n_13_32111_31_0_ > tests/test_decod1_email.txt
+fstcompile --isymbols=syms.sym --osymbols=syms.sym  tests/test_decod1_email.txt | fstarcsort > tests/test_decod1_email.fst
+
+python word2fst.py 3332111_3412_321_n_13_2111_321_0_3311_d9_jXn9Vr0_p9lXs_312_h_ > tests/test_decod2_email.txt
+fstcompile --isymbols=syms.sym --osymbols=syms.sym  tests/test_decod2_email.txt | fstarcsort > tests/test_decod2_email.fst
+
+
 #################### Testing Transducer 1 ####################
 fstcompose tests/test_99_.fst transducers/transducer1.fst | fstrmepsilon > results/result_t1_XCIX_.fst
 fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_t1_XCIX_.fst | dot -Tpdf  > results/result_t1_XCIX_.pdf
@@ -175,6 +185,9 @@ fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_cod
 fstcompose tests/test_ir_tambem_.fst transducers/tranducer_cod.fst | fstrmepsilon > results/result_cod_Vr_tX79m_.fst
 fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_cod_Vr_tX79m_.fst | dot -Tpdf  > results/result_cod_Vr_tX79m_.pdf
 
+fstcompose tests/test_cod_email.fst transducers/tranducer_cod.fst | fstproject --project_output | fstrmepsilon > results/result_cod_email.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_cod_email.fst | dot -Tpdf  > results/result_cod_email.pdf
+
 ################ Testing Decodifier Transducer ###############
 fstcompose tests/test_3513_.fst transducers/tranducer_decod.fst | fstrmepsilon > results/result_decod_3513_.fst
 fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_decod_3513_.fst | dot -Tpdf  > results/result_decod_3513_.pdf
@@ -187,5 +200,11 @@ fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_dec
 
 fstcompose tests/test_Vr_tX79m_.fst transducers/tranducer_decod.fst | fstrmepsilon > results/result_decod_ir_tambem.fst
 fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_decod_ir_tambem.fst | dot -Tpdf  > results/result_decod_ir_tambem.pdf
+
+fstcompose tests/test_decod1_email.fst transducers/tranducer_decod.fst | fstproject --project_output | fstrmepsilon > results/result_decod1_email.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_decod1_email.fst | dot -Tpdf  > results/result_decod1_email.pdf
+
+fstcompose tests/test_decod2_email.fst transducers/tranducer_decod.fst | fstproject --project_output | fstrmepsilon > results/result_decod2_email.fst
+fstdraw    --isymbols=syms.sym --osymbols=syms.sym --portrait results/result_decod2_email.fst | dot -Tpdf  > results/result_decod2_email.pdf
 
 ##############################################################
